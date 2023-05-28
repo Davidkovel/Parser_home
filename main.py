89913e58de0df7f91218a4e85561e124fc197e11
David <dkovel7@gmail.com>
2023-05-28 16:28:09 +0100

    task_1

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1
+
+from bs4 import BeautifulSoup
+import requests
+
+def parser():
+
+    try:
+        url = "https://www.example.com"
+
+        responce = requests.get(url)
+        soup = BeautifulSoup(responce.content, 'html.parser')
+        items = soup.find_all('h1')
+
+        for item in items:
+            print(item.text)
+    except:
+        print("Немаэ пiдключення", "Error 404")
+
+parser()
